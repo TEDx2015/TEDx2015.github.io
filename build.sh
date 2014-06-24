4e2a613e5fe3b7c78f942d6de71fec97cd87e4f6
Hardmath123 <hardmath123@gmail.com>
2014-06-24 12:22:57 -0700

    started using mustache, created multiple pages

---
+++ build.sh
@@ -0,0 +1,5 @@
+mustache index.mustache > index.html
+mustache about.mustache > about.html
+mustache sponsors.mustache > sponsors.html
+mustache contact.mustache > contact.html
+echo "Built at $(date)"
