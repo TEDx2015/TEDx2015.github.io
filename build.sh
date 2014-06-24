bc9faf3fe8368a10409e68bdc103989032f9d523
Hardmath123 <hardmath123@gmail.com>
2014-06-24 12:35:37 -0700

    files

--- build.sh
+++ build.sh
@@ -1,5 +1,8 @@
-mustache index.mustache > index.html
-mustache about.mustache > about.html
-mustache sponsors.mustache > sponsors.html
-mustache contact.mustache > contact.html
+cd templates
+mustache index.mustache    > ../index.html
+mustache about.mustache    > ../about.html
+mustache sponsors.mustache > ../sponsors.html
+mustache contact.mustache  > ../contact.html
+mustache speakers.mustache > ../speakers.html
 echo "Built at $(date)"
+# watch -d -n 1 ./build.sh
