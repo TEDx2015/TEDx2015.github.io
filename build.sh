a3c0a4a88725705c23a2243388e078da70767697
Sam Winslow <sammywinslow@gmail.com>
2014-09-03 21:28:46 -0700

    Added team page and moved social icons

--- build.sh
+++ build.sh
@@ -3,5 +3,6 @@ mustache index.mustache    > ../index.html
 mustache about.mustache    > ../about.html
 mustache sponsors.mustache > ../sponsors.html
 mustache speakers.yml speakers.mustache > ../speakers.html
+mustache team.mustache     > ../team.html
 echo "Built at $(date)"
 # watch -d -n 1 ./build.sh
