29d4292a11489fc92cb88149b8fdd4a41055a90a
Sam Winslow <sammywinslow@gmail.com>
2014-09-12 06:54:27 -0700

    Added media page

--- build.sh
+++ build.sh
@@ -4,5 +4,6 @@ mustache about.mustache    > ../about.html
 mustache sponsors.mustache > ../sponsors.html
 mustache speakers.yml speakers.mustache > ../speakers.html
 mustache team.mustache     > ../team.html
+mustache media.mustache     > ../media.html
 echo "Built at $(date)"
 # watch -d -n 1 ./build.sh
