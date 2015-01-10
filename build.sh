25ea1592c2be822f53b5aa669914f20e9acf755d
Hardmath123 <hardmath123@gmail.com>
2015-01-10 10:53:01 -0800

    whoo all sorts of changes all over the place

--- build.sh
+++ build.sh
@@ -3,8 +3,8 @@ mustache head.mustache     > ../head.html
 mustache index.mustache    > ../index.html
 mustache about.mustache    > ../about.html
 mustache sponsors.mustache > ../sponsors.html
-# mustache speakers.yml speakers.mustache > ../speakers.html
+mustache speakers.mustache > ../speakers.html
 mustache team.mustache     > ../team.html
-mustache media.mustache     > ../media.html
+mustache media.mustache    > ../media.html
 echo "Built at $(date)"
 # watch -d -n 1 ./build.sh
