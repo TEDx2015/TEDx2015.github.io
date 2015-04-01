a4c5041eb78abdd41cd00f0c9c7d0050bc37c12e
Sam Winslow <sammywinslow@gmail.com>
2015-03-31 21:09:01 -0700

    Forgot to commit after last changes; misc stuff

--- build.sh
+++ build.sh
@@ -3,7 +3,6 @@ mustache head.mustache     > ../head.html
 mustache index.mustache    > ../index.html
 mustache about.mustache    > ../about.html
 mustache sponsors.mustache > ../sponsors.html
-mustache speakers.mustache > ../speakers.html
 mustache team.mustache     > ../team.html
 mustache media.mustache    > ../media.html
 echo "Built at $(date)"
