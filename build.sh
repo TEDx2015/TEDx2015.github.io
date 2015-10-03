6583f031b90d956dbecb23b61e22faa48ab63d98
gmittal <gautam@mittal.net>
2015-10-03 16:22:22 -0700

    refactored the code

--- build.sh
+++
@@ -1,9 +0,0 @@
-cd templates
-mustache head.mustache     > ../head.html
-mustache index.mustache    > ../index.html
-mustache about.mustache    > ../about.html
-mustache sponsors.mustache > ../sponsors.html
-mustache team.mustache     > ../team.html
-mustache media.mustache    > ../media.html
-echo "Built at $(date)"
-# watch -d -n 1 ./build.sh
